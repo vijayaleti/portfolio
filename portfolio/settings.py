7297dfee56e7262161fe6b2ee3353110d61f96fb
vijayaleti <vijayaleti1@gmail.com>
2020-10-01 17:09:58 +0530

    Updated all Blog and Jobs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS=[
+      BASE_DIR/'portfolio\static'
+]
+STATIC_ROOT = BASE_DIR /'static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
